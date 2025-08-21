4d714cd11f84bea9f31866316f9ded469cfb00e3
Nasim <mnasimhosen11@gmail.com>
2025-08-22 02:16:27 +0600

    code updated

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,5 +1,5 @@
-import { Webhook } from "svix";
-import { headers } from "next/headers";
+import { Webhook } from 'svix';
+import { headers } from 'next/headers';
 
 export async function POST(req) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
@@ -7,19 +7,19 @@ export async function POST(req) {
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
-      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+      'Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local'
     );
   }
 
   // Get the headers
   const headerPayload = headers();
-  const svix_id = headerPayload.get("svix-id");
-  const svix_timestamp = headerPayload.get("svix-timestamp");
-  const svix_signature = headerPayload.get("svix-signature");
+  const svix_id = headerPayload.get('svix-id');
+  const svix_timestamp = headerPayload.get('svix-timestamp');
+  const svix_signature = headerPayload.get('svix-signature');
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
+    return new Response('Error occured -- no svix headers', {
       status: 400,
     });
   }
@@ -36,13 +36,13 @@ export async function POST(req) {
   // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
-      "svix-id": svix_id,
-      "svix-timestamp": svix_timestamp,
-      "svix-signature": svix_signature,
+      'svix-id': svix_id,
+      'svix-timestamp': svix_timestamp,
+      'svix-signature': svix_signature,
     });
   } catch (err) {
-    console.error("Error verifying webhook:", err);
-    return new Response("Error occured", {
+    console.error('Error verifying webhook:', err);
+    return new Response('Error occured', {
       status: 400,
     });
   }
@@ -52,15 +52,15 @@ export async function POST(req) {
   const { id } = evt.data;
   const eventType = evt.type;
   console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
-  console.log("Webhook body:", body);
+  console.log('Webhook body:', body);
 
-  if (evt.type === "user.created") {
-    console.log("userId:", evt.data.id);
+  if (evt.type === 'user.created') {
+    console.log('userId:', evt.data.id);
   }
 
-  if (evt.type === "user.updated") {
-    console.log("user is updated:", evt.data.id);
+  if (evt.type === 'user.updated') {
+    console.log('user is updated:', evt.data.id);
   }
 
-  return new Response("", { status: 200 });
+  return new Response('', { status: 200 });
 }
